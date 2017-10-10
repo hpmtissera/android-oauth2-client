c285b9cd01afc8abe0733f6e02ceb95aade2093d
Prasad Tissera <hpmtissera@gmail.com>
2017-10-10 18:08:05 +0900

    Update ExampleUnitTest.java

--- app/src/test/java/com/lanka_guide/oauth2client/ExampleUnitTest.java
+++ app/src/test/java/com/lanka_guide/oauth2client/ExampleUnitTest.java
@@ -1,3 +1,4 @@
+/*
 package com.lanka_guide.oauth2client;
 
 import org.junit.Test;
@@ -9,9 +10,11 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+/*
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
 }
+*/
